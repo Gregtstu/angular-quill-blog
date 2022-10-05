a964bda5b0714a330be8911667b90ad9a9b0c287
Gregtstu <boltnev.grisha@yandex.ru>
2022-10-05 21:04:25 +0300

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { QuillModule } from 'ngx-quill';
+import { MainLayoutComponent } from './user/shared/main-layout/main-layout.component';
+import { PostPageComponent } from './user/post-page/post-page.component';
+import { CreatePageComponent } from './user/create-page/create-page.component';
+import { FavoritePageComponent } from './user/favorite-page/favorite-page.component';
+import { MainPageComponent } from './user/main-page/main-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainLayoutComponent,
+    PostPageComponent,
+    CreatePageComponent,
+    FavoritePageComponent,
+    MainPageComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    QuillModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
