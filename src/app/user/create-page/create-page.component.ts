f2b7a0882dbfa29957b3e8b3d012980859b0f9a6
Gregtstu <boltnev.grisha@yandex.ru>
2022-10-07 16:20:18 +0300

    fixed bags

--- src/app/user/create-page/create-page.component.ts
+++ src/app/user/create-page/create-page.component.ts
@@ -13,13 +13,13 @@ export class CreatePageComponent implements OnInit {
     public quillConfig = {
       toolbar: [
         ['bold', 'italic', 'underline', 'strike'],        // toggled buttons
-        ['blockquote', 'code-block'],
+        // ['blockquote', 'code-block'],
 
         [{ 'header': 1 }, { 'header': 2 }],               // custom button values
         [{ 'list': 'ordered'}, { 'list': 'bullet' }],
         [{ 'script': 'sub'}, { 'script': 'super' }],      // superscript/subscript
         [{ 'indent': '-1'}, { 'indent': '+1' }],          // outdent/indent
-        [{ 'direction': 'rtl' }],                         // text direction
+        // [{ 'direction': 'rtl' }],                         // text direction
 
         [{ 'size': ['small', false, 'large', 'huge'] }],  // custom dropdown
         [{ 'header': [1, 2, 3, 4, 5, 6, false] }],
